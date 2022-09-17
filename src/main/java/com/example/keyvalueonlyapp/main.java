80ec59cc662904f95e8c28d92b37eb79bc396b6d
Yandi Bastian <yandi.bastian@klikdokter.com>
2022-09-17 19:21:50 +0700

    add new feature

--- src/main/java/com/example/keyvalueonlyapp/main.java
+++ src/main/java/com/example/keyvalueonlyapp/main.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 @RestController
@@ -19,12 +20,25 @@ public class main {
     public ResponseEntity<Response> get(@PathVariable("id") String id) {
         Response response = new Response();
         String allowedId = this.AllowedId;
-        HashMap<String, Boolean> mappingSuccess = new HashMap<String, Boolean>() {{
-            put(allowedId, true);
-        }};
+        HashMap<String, Boolean> mappingSuccess = new HashMap<String, Boolean>();
 
-        String messageResps = id.equals(allowedId) ? "Allowed" : "Denied";
-        Boolean successResps = mappingSuccess.get(id) != null;
+        if (allowedId.contains(",")){
+            String[] newAllowedId = allowedId.split(",");
+            for (String i : newAllowedId) {
+                mappingSuccess.put(i, true);
+            }        
+        }
+
+        String messageResps;
+        Boolean successResps;
+
+        if (mappingSuccess.get(id) != null) {
+            messageResps = "Allowed";
+            successResps = true;
+        }else {
+            messageResps = "Denied";
+            successResps = false;
+        }
 
         response.setData("OK");
         response.setSuccess(successResps);
@@ -39,6 +53,7 @@ public class main {
     @GetMapping
     public ResponseEntity<Response> get() {
         Response response = new Response();
+
 
         response.setData("Hello");
         response.setSuccess(true);
